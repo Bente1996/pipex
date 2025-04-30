0ea4e74be8dec6c12fb39dcec57198d70cb6e4f5
Bente1996 <bentedekoning@gmail.com>
2025-04-30 16:36:39 +0200

    voor get_next_line erbij

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: bde-koni <bde-koni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 19:40:54 by bde-koni          #+#    #+#             */
-/*   Updated: 2025/04/29 19:02:55 by bde-koni         ###   ########.fr       */
+/*   Updated: 2025/04/30 16:31:55 by bde-koni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-void	pipex(char *file1, char *cmd1, char *cmd2, char *file2) // parent process
+void	pipex(char *file1, char *cmd1, char *cmd2, char *file2) // parent process, ** ipv *?
 {
 	int	pipefd[2]; // array of 2 fd's (read(0) and write(1) end)
 	pid_t	pid;
 	int	fd1; // file1
 	int	fd2; //file2
-	cmd1 = "";
+	cmd1 = "cat";
 	cmd2 = "";
 	cmd2 = cmd1;
 	cmd1 = cmd2;
@@ -28,7 +28,7 @@ void	pipex(char *file1, char *cmd1, char *cmd2, char *file2) // parent process
 	fd1 = open(file1, O_RDONLY); //read file that is first parameter
 	fd2 = open(file2, O_WRONLY | O_CREAT | O_TRUNC, 0644); // write in file that is last parameter
 	
-	pipe(pipefd); // create pipe ([0] and [1]) (pipe is zelde in beide childs)
+	pipe(pipefd); // create pipe ([0] and [1]) (pipe is zelfde in beide childs)
 	pid = fork(); // make child
 	
 	if (pid == 0) // doe dit in de child process (cmd 1 and write to pipe)
@@ -37,16 +37,32 @@ void	pipex(char *file1, char *cmd1, char *cmd2, char *file2) // parent process
 		printf("%d\n", fd1); // = -1 nu
 		dup2(pipefd[1], STDOUT_FILENO); // (write end / write to fd[1] is now stdout)
 		close(pipefd[0]); // close unsused read end
-		//execve(cmd1[0], cmd1, NULL); // switch to new program (pathname, parameters, envp)
+		change_program(cmd1);
 	}
 	else // doe dit in de parent process (cmd 2 and read from pipe)
 	{
 		dup2(pipefd[0], STDIN_FILENO); // (read end / read from fd[0] is now stdin)
-		dup2(fd2, STDOUT_FILENO); // write to output file
+		dup2(fd2, STDOUT_FILENO); // stdout is nu fd2
 		close(pipefd[1]); // close unused write end
-		
-		//execve(cmd1[0], cmd1, NULL);
+		change_program(cmd2);
 	}
+}
+
+char	*find_cmd_path(char **cmd_args)
+{
+	PATH = envp;
+	get_next_line(envp); // lees PATH line voor line
+}
+
+void	change_program(char *cmd) // "cat -e"
+{
+	char	*path;
+	char	**cmd_args;
+	
+	cmd_args = ft_split(cmd, ' '); // breek command in stukken
+	path = find_cmd_path(cmd_args);
+	execve(path, cmd_args, envp);
+	
 }
 
 int	main(int argc, char **argv)
