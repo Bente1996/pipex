7d519381f08ca02f8691dc712c7342130d3e8444
Bente1996 <bentedekoning@gmail.com>
2025-05-08 19:31:00 +0200

    einde 8 mei

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: bde-koni <bde-koni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 19:40:54 by bde-koni          #+#    #+#             */
-/*   Updated: 2025/05/08 19:07:39 by bde-koni         ###   ########.fr       */
+/*   Updated: 2025/05/08 19:28:14 by bde-koni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,10 @@ void	pipex(char *file1, char *cmd1, char *cmd2, char *file2, char **envp) // par
 	
 	fd1 = open(file1, O_RDONLY); //read file that is first parameter
 	fd2 = open(file2, O_WRONLY | O_CREAT | O_TRUNC, 0644); // write in file that is last parameter
-	if (fd1 < 0 || fd2 < 0)
-		exit_with_failure("open failed\n", 1, 1);
+	if (fd1 < 0)
+		exit_with_failure("opening file1 failed\n", 1, 1);
+	if (fd2 < 0)
+		exit_with_failure("opening file2 failed\n", 1, 1);
 	if (pipe(pipefd) == -1) // create pipe ([0] and [1]) (pipe is zelfde in beide childs)
 		exit_with_failure("pipe failed\n", 1, 1);
 	pid = fork(); // make child
@@ -46,57 +48,32 @@ void	pipex(char *file1, char *cmd1, char *cmd2, char *file2, char **envp) // par
 		dup2(pipefd[1], STDOUT_FILENO); // stdout wijst naar write end van pipe
 		close(pipefd[0]); // close unsused read end
 		close(pipefd[1]);
-		close(fd2);
 		change_program(cmd1, envp); //geef command mee en voer uit op juiste file met execve
 	}
 	else // doe dit in de parent process (cmd 2 and read from pipe)
 	{
 		int	status; // integer die status info opslaat met behuld van macro's
-
+		
 		dup2(pipefd[0], STDIN_FILENO); // (read end / read from fd[0] is now stdin)
 		dup2(fd2, STDOUT_FILENO); // stdout is nu fd2
 		close(pipefd[1]); // close unused write end
 		close(pipefd[0]);
-		close(fd1);
 		waitpid(pid, &status, 0);
 		if (WIFEXITED(status))
-			ft_printf("Child exited with this exit code: %d\n", WEXITSTATUS(status));
+		ft_printf("Child exited with this exit code: %d\n", WEXITSTATUS(status));
 		else if (WIFSIGNALED(status))
-			ft_printf("Child was killed by this signal: %d\n", WTERMSIG(status));
+		ft_printf("Child was killed by this signal: %d\n", WTERMSIG(status));
 		change_program(cmd2, envp);
 	}
-}
-void	exit_with_failure(char *message, int use_perror, int error_code)
-{
-	if (use_perror)
-		perror(message);
-	else
-		ft_putendl_fd(message, 2); // write to stderr
-	exit(error_code);
+	close(fd1);
+	close(fd2);
 }
 
-void	pipex(char *file1, char *cmd1, char *cmd2, char *file2, char **envp) // parent process, ** ipv *?
+char	*find_path_line(char **envp) // opzoek naar PATH= in envp
 {
-	int	pipefd[2]; // array of 2 fd's (read(0) and write(1) end)
-	pid_t	pid;
-	int	fd1; // file1
-	int	fd2; //file2
-	
-	fd1 = open(file1, O_RDONLY); //read file that is first parameter
-	fd2 = open(file2, O_WRONLY | O_CREAT | O_TRUNC, 0644); // write in file that is last parameter
-	if (fd1 < 0 || fd2 < 0)
-		exit_with_failure("open failed\n", 1, 1);
-	if (pipe(pipefd) == -1) // create pipe ([0] and [1]) (pipe is zelfde in beide childs)
-		exit_with_failure("pipe failed\n", 1, 1);
-	pid = fork(); // make child
-	if (pid == 0) // doe dit in de child process (cmd 1 and write to pipe)
-	{
-		dup2(fd1, STDIN_FILENO); // stdin wijst naar file1 (oldfd, newfd)  NIEUW WIJST NAR OUD
-		//printf("%d\n", fd1); // = -1 nu
-		dup2(pipefd[1], STDOUT_FILENO); // stdout wijst naar write end van pipe
-		close(pipefd[0]); // close unsused read end
-		close(pipefd[1]);
-		close(fd2);
+	int	i;
+
+	i = 0;
 	while (envp[i] != NULL)
 	{
 		if (ft_strncmp("PATH=", envp[i], 5) == 0)
@@ -153,15 +130,17 @@ void	change_program(char *cmd, char **envp) // "cat -e"
 	char	*full_path;
 	
 	cmd_args = ft_split(cmd, ' '); // breek command in stukken
+	if (!cmd_args)
+		exit_with_failure("split cmd_args failed\n", 1, 1);
 	path_line = find_path_line(envp); // zoek naar PATH in envp
 	paths = ft_split(path_line, ':'); // split bij :
+	if (!paths)
+		exit_with_failure("split paths failed\n", 1, 1);
 	full_path = find_path(paths, cmd_args[0]);
 	if (!full_path)
 	{
 		free_split(cmd_args);
 		exit_with_failure("command not found\n", 0, 127);
-		// ft_putendl_fd("command not found\n", 2); // dont use perror because no system fail
-		// exit(127); // 127: error code for command not found
 	}
 	if (execve(full_path, cmd_args, envp) == -1) // niet nodig on succeed want dan wordt alles gewiped door nieuwe programma
 	{
@@ -174,12 +153,7 @@ void	change_program(char *cmd, char **envp) // "cat -e"
 int	main(int argc, char **argv, char **envp)
 {
 	if (argc != 5)
-	exit_with_failure("invalid amount of arguments\n", 0, 2);
-	// {
-	// 	fd_putendl_fd("invalid amount of arguments\n", 2);
-	// 	exit(2);
-	// }
-		// return (1);
+		exit_with_failure("invalid amount of arguments\n", 0, 2);
 	pipex(argv[1], argv[2], argv[3], argv[4], envp);
 	return(0);
 }
