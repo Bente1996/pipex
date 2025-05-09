29cc1d3c811166572c28d5196c9f2a855b5deee3
Bente1996 <bentedekoning@gmail.com>
2025-05-09 20:17:33 +0200

    einde 9 mei

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: bde-koni <bde-koni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 19:40:54 by bde-koni          #+#    #+#             */
-/*   Updated: 2025/05/08 19:28:14 by bde-koni         ###   ########.fr       */
+/*   Updated: 2025/05/09 20:16:57 by bde-koni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#define READ 0
+#define WRITE 1
 
 void	exit_with_failure(char *message, int use_perror, int error_code)
 {
@@ -25,12 +27,33 @@ void	exit_with_failure(char *message, int use_perror, int error_code)
 	exit(error_code);
 }
 
+pid_t	ft_fork()
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == -1)
+		exit_with_failure("forking went wrong\n", 1, 1);
+	return (pid);
+}
+
+pid_t	ft_waitpid(pid_t pid, int *status, int options)
+{
+	waitpid(pid, status, options);
+	if (WIFEXITED(*status)) // * want kijken naar de waarde op het adres
+		ft_printf("Child exited with this exit code: %d\n", WEXITSTATUS(*status));
+	else if (WIFSIGNALED(*status))
+		ft_printf("Child was killed by this signal: %d\n", WTERMSIG(*status));
+}
+
 void	pipex(char *file1, char *cmd1, char *cmd2, char *file2, char **envp) // parent process, ** ipv *?
 {
 	int	pipefd[2]; // array of 2 fd's (read(0) and write(1) end)
-	pid_t	pid;
+	pid_t	pid1;
+	pid_t	pid2;
 	int	fd1; // file1
 	int	fd2; //file2
+	int	status[2]; // integer die status info opslaat met behuld van macro's
 	
 	fd1 = open(file1, O_RDONLY); //read file that is first parameter
 	fd2 = open(file2, O_WRONLY | O_CREAT | O_TRUNC, 0644); // write in file that is last parameter
@@ -40,31 +63,27 @@ void	pipex(char *file1, char *cmd1, char *cmd2, char *file2, char **envp) // par
 		exit_with_failure("opening file2 failed\n", 1, 1);
 	if (pipe(pipefd) == -1) // create pipe ([0] and [1]) (pipe is zelfde in beide childs)
 		exit_with_failure("pipe failed\n", 1, 1);
-	pid = fork(); // make child
-	if (pid == 0) // doe dit in de child process (cmd 1 and write to pipe)
+	pid1 = ft_fork(); // make child
+	if (pid1 == 0) // doe dit in de child process (cmd 1 and write to pipe)
 	{
 		dup2(fd1, STDIN_FILENO); // stdin wijst naar file1 (oldfd, newfd)  NIEUW WIJST NAR OUD
 		//printf("%d\n", fd1); // = -1 nu
-		dup2(pipefd[1], STDOUT_FILENO); // stdout wijst naar write end van pipe
+		dup2(pipefd[WRITE], STDOUT_FILENO); // stdout wijst naar write end van pipe
 		close(pipefd[0]); // close unsused read end
 		close(pipefd[1]);
 		change_program(cmd1, envp); //geef command mee en voer uit op juiste file met execve
 	}
-	else // doe dit in de parent process (cmd 2 and read from pipe)
+	pid2 = ft_fork();
+	if (pid2 == 0)
 	{
-		int	status; // integer die status info opslaat met behuld van macro's
-		
 		dup2(pipefd[0], STDIN_FILENO); // (read end / read from fd[0] is now stdin)
 		dup2(fd2, STDOUT_FILENO); // stdout is nu fd2
 		close(pipefd[1]); // close unused write end
 		close(pipefd[0]);
-		waitpid(pid, &status, 0);
-		if (WIFEXITED(status))
-		ft_printf("Child exited with this exit code: %d\n", WEXITSTATUS(status));
-		else if (WIFSIGNALED(status))
-		ft_printf("Child was killed by this signal: %d\n", WTERMSIG(status));
 		change_program(cmd2, envp);
 	}
+	ft_waitpid(pid1, &status[0], 0); //& omdat je niet nummer maar adres wil meegeven
+	ft_waitpid(pid2, &status[1], 0); // doe dit in parent om error codes op te vangen van execve
 	close(fd1);
 	close(fd2);
 }
@@ -108,7 +127,18 @@ char	*find_path(char **paths, char *cmd) // vind volledige path
 	while (paths[i] != NULL) // maak volledige paths
 	{
 		add_slash = ft_strjoin(paths[i], "/"); // we maken een 3e string /bin + "/" = /bin/
+		if (!add_slash)
+		{
+			free_split(paths);
+			exit_with_failure("strjoin malloc fail", 1, 1);
+		}
 		full_path = ft_strjoin(add_slash, cmd); // we maken een 4e string /bin/cat
+		if (!full_path)
+		{
+			free(full_path);
+			free_split(paths);
+			exit_with_failure("strjoin malloc fail", 1, 1);
+		}
 		free(add_slash); // verwijder 3e string
 		if (access(full_path ,X_OK) == 0) // als we gevonden: geef execute recht
 		{
