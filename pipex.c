56e504199dcb6e334559d49a86cb8e7f908e160d
Bente de Koning <bentdekoning@gmail.com>
2025-05-11 20:38:31 +0200

    child 1 en 2 en fd_open

--- pipex.c
+++ pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bde-koni <bde-koni@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bde-koni <bde-koni@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 19:40:54 by bde-koni          #+#    #+#             */
-/*   Updated: 2025/05/09 20:16:57 by bde-koni         ###   ########.fr       */
+/*   Updated: 2025/05/11 20:37:06 by bde-koni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,11 +39,44 @@ pid_t	ft_fork()
 
 pid_t	ft_waitpid(pid_t pid, int *status, int options)
 {
-	waitpid(pid, status, options);
+	pid = waitpid(pid, status, options);
 	if (WIFEXITED(*status)) // * want kijken naar de waarde op het adres
 		ft_printf("Child exited with this exit code: %d\n", WEXITSTATUS(*status));
 	else if (WIFSIGNALED(*status))
 		ft_printf("Child was killed by this signal: %d\n", WTERMSIG(*status));
+	return (pid);
+}
+
+int	open_fd(int file, int oflag)
+{
+	if (oflag == READ)
+		file = open(file, O_RDONLY); //read file that is first parameter
+	else
+		file = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644); // write in file that is last parameter
+	if (file < 0 && oflag == READ)
+		exit_with_failure("opening file1 failed\n", 1, 1);
+	else if (file < 0)
+		exit_with_failure("opening file2 failed\n", 1, 1);
+	return (file);
+}
+
+void	child_1(int *pipefd, char *file1, char *cmd1, char **envp)
+{
+	dup2(file1, STDIN_FILENO); // stdin wijst naar file1 (oldfd, newfd)  NIEUW WIJST NAR OUD
+	//printf("%d\n", fd1); // = -1 nu
+	dup2(pipefd[WRITE], STDOUT_FILENO); // stdout wijst naar write end van pipe
+	close(pipefd[READ]); // close unsused read end
+	close(pipefd[WRITE]);
+	change_program(cmd1, envp); //geef command mee en voer uit op juiste file met execve
+}
+
+void	child_2(int *pipefd, char *file2, char *cmd2, char **envp)
+{
+	dup2(pipefd[READ], STDIN_FILENO); // (read end / read from fd[0] is now stdin)
+	dup2(file2, STDOUT_FILENO); // stdout is nu fd2
+	close(pipefd[WRITE]); // close unused write end
+	close(pipefd[READ]);
+	change_program(cmd2, envp);
 }
 
 void	pipex(char *file1, char *cmd1, char *cmd2, char *file2, char **envp) // parent process, ** ipv *?
@@ -55,33 +88,16 @@ void	pipex(char *file1, char *cmd1, char *cmd2, char *file2, char **envp) // par
 	int	fd2; //file2
 	int	status[2]; // integer die status info opslaat met behuld van macro's
 	
-	fd1 = open(file1, O_RDONLY); //read file that is first parameter
-	fd2 = open(file2, O_WRONLY | O_CREAT | O_TRUNC, 0644); // write in file that is last parameter
-	if (fd1 < 0)
-		exit_with_failure("opening file1 failed\n", 1, 1);
-	if (fd2 < 0)
-		exit_with_failure("opening file2 failed\n", 1, 1);
+	fd1 = open_fd(file1, READ);
+	fd2 = open_fd(file2, WRITE);
 	if (pipe(pipefd) == -1) // create pipe ([0] and [1]) (pipe is zelfde in beide childs)
 		exit_with_failure("pipe failed\n", 1, 1);
 	pid1 = ft_fork(); // make child
 	if (pid1 == 0) // doe dit in de child process (cmd 1 and write to pipe)
-	{
-		dup2(fd1, STDIN_FILENO); // stdin wijst naar file1 (oldfd, newfd)  NIEUW WIJST NAR OUD
-		//printf("%d\n", fd1); // = -1 nu
-		dup2(pipefd[WRITE], STDOUT_FILENO); // stdout wijst naar write end van pipe
-		close(pipefd[0]); // close unsused read end
-		close(pipefd[1]);
-		change_program(cmd1, envp); //geef command mee en voer uit op juiste file met execve
-	}
+		child_1(&pipefd, file1, cmd1, envp);
 	pid2 = ft_fork();
 	if (pid2 == 0)
-	{
-		dup2(pipefd[0], STDIN_FILENO); // (read end / read from fd[0] is now stdin)
-		dup2(fd2, STDOUT_FILENO); // stdout is nu fd2
-		close(pipefd[1]); // close unused write end
-		close(pipefd[0]);
-		change_program(cmd2, envp);
-	}
+		child_2(&pipefd, file2, cmd2, envp);
 	ft_waitpid(pid1, &status[0], 0); //& omdat je niet nummer maar adres wil meegeven
 	ft_waitpid(pid2, &status[1], 0); // doe dit in parent om error codes op te vangen van execve
 	close(fd1);
@@ -140,7 +156,7 @@ char	*find_path(char **paths, char *cmd) // vind volledige path
 			exit_with_failure("strjoin malloc fail", 1, 1);
 		}
 		free(add_slash); // verwijder 3e string
-		if (access(full_path ,X_OK) == 0) // als we gevonden: geef execute recht
+		if (access(full_path ,X_OK) == 0) // als gevonden: geef execute recht
 		{
 			free_split(paths); // free alle OG paths, "/" hoeft niet
 			return (full_path);
